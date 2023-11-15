3ae5c51ea3e9c12d03437f160fe6dfa5cd91c9e7
nilbace <nilbace@naver.com>
2023-11-15 15:55:43 +0900

    점검

--- Assets/Scripts/UI/Scene/UI_MainBackUI.cs
+++ Assets/Scripts/UI/Scene/UI_MainBackUI.cs
@@ -249,7 +249,7 @@ public class UI_MainBackUI : UI_Scene
     /// </summary>
     public void EndScheduleAndSetUI()
     {
-        Debug.Log($"{Managers.Data._myPlayerData.NowWeek -1 } / 구독자_{Managers.Data._myPlayerData.nowSubCount} / 노래 스텟_{Managers.Data._myPlayerData.SixStat[1]}");
+        Debug.Log($"{Managers.Data._myPlayerData.NowWeek -1 } / 구독자_{Managers.Data._myPlayerData.nowSubCount} / 게임 스텟_{Managers.Data._myPlayerData.SixStat[0]}");
         StartCoroutine(EndScheduleAndSetUICor());
     }
 
