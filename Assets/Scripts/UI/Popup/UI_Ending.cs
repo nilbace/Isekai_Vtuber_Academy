e7c8354464ffeb1e661a3a9f071e8d124085922e
nilbace <101455214+nilbace@users.noreply.github.com>
2024-01-02 20:03:16 +0900

    엔딩 2차 수정 요망

--- Assets/Scripts/UI/Popup/UI_Ending.cs
+++ Assets/Scripts/UI/Popup/UI_Ending.cs
@@ -1,16 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UI_Ending : UI_Popup
 {
-    void Start()
+    enum Buttons
     {
-        
+        NextBTN,
+        FinishBTN
+    }
+    enum Texts
+    {
+        EndingNameTMP,
+        EndingIndexTMP,
+        EndingTextTMP
     }
 
-    void Update()
+    enum Images {
+        EndingIMG
+    }
+
+    private void Start()
     {
+        Init();
+    }
+
+    public override void Init()
+    {
+        base.Init();
+        Bind<Button>(typeof(Buttons));
+        Bind<Image>(typeof(Images));
+        Bind<TMPro.TMP_Text>(typeof(Texts));
+
         
     }
 }
